58fa9f5e2350fea54e38c5c1430ff19519a0824a
Waldemar <94298588+WSF99@users.noreply.github.com>
2023-04-06 18:15:20 -0300

    add problem

---
+++ count_and_say.py
@@ -0,0 +1,21 @@
+# Problem: https://leetcode.com/problems/count-and-say/
+
+class Solution:
+    def count_and_say(self, n: int) -> str:
+        if n < 1 or n > 30:
+            return ""
+        if n == 1:
+            return "1"
+        say = self.countAndSay(n - 1)
+
+        new_string = ""
+        cur = say[0]
+        cnt = 0
+        for num in say:
+            if num == cur:
+                cnt += 1
+            else:
+                new_string += str(cnt) + cur
+                cnt = 1
+                cur = num
+        return new_string + str(cnt) + cur
